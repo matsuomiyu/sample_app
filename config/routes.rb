a8ed1b14020909ff91da8807439d26a30c376568
matsuomiyu <miyou0210@gmail.com>
2024-05-13 05:28:25 +0000

    [start]4-4

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :lists
 
   get 'lists/new'
-  get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
   get 'top' => 'homes#top'
-
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
