7d6f37b8442a4d2e82f7c8aa1194cf5c275ad3f6
matsuomiyu <miyou0210@gmail.com>
2024-05-17 09:49:48 +0000

    [start]4-10

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,6 +15,7 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
+  
 
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
@@ -22,7 +23,7 @@ class ListsController < ApplicationController
     # 3. データをデータベースに保存するためのsaveメソッド実行
     if @list.save
     # 4. トップ画面へリダイレクト
-     redirect_to list_path(@list)
+     redirect_to list_path(@list.id)
     else
      render :new
     end
@@ -32,6 +33,12 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
    @list.update(list_params)
    redirect_to list_path(@list.id)
+  end
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
   end
   private
   # ストロングパラメータ
